166b7c38739cdc81ec0f7435777e46a63b8f4ceb
Giang Le <gianghl2@illinois.edu>
2019-11-23 01:35:23 -0600

    Clicking MY CLOSET will move the user to another screen.
    Signed-off-by: Giang Le <gianghl2@illinois.edu>

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button myClosetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myClosetButton = (Button) findViewById(R.id.myCloset);
+        myClosetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMyCloset();
+            }
+        });
+    }
+
+    public void openMyCloset() {
+        Intent intent = new Intent(this, MyCloset.class);
+        startActivity(intent);
     }
 }
