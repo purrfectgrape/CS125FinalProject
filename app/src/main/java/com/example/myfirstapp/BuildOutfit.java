26393c22434ac8d782158eb300dd722497b72001
Giang Le <gianghl2@illinois.edu>
2019-11-25 23:46:38 -0600

    Adding preliminary weather and mood UI
    Signed-off-by: Giang Le <gianghl2@illinois.edu>

--- app/src/main/java/com/example/myfirstapp/BuildOutfit.java
+++ app/src/main/java/com/example/myfirstapp/BuildOutfit.java
@@ -2,13 +2,58 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BuildOutfit extends AppCompatActivity {
+
+    /**
+     * Button backButton - reference to the backButton.
+     */
+    private Button backButton;
+
+    /**
+     * Button weather - reference to the weather Button.
+     */
+//    private Button weather;
+
+    /**
+     * Button mood - reference to the mood Button.
+     * Picture was taken from https://icon-library.net/icon/mood-icon-0.html.
+     */
+//    private Button mood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build_outfit);
+
+        backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goBack();
+            }
+        });
+
+//        weather = (Button) findViewById(R.id.weather);
+//        weather.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                goToWeather();
+//            }
+//        });
     }
+
+    public void goBack() {
+        Intent intentGoBack = new Intent(this, MainActivity.class);
+        startActivity(intentGoBack);
+    }
+
+//    public void goToWeather() {
+//        Intent intentGoToWeather = new Intent(this, Weather.class);
+//        startActivity(intentGoToWeather);
+//    }
 }
