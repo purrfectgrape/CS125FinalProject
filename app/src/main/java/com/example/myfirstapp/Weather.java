db10ceac9afefe6ec22d8c7eb482e9edac5e7a0d
Giang Le <gianghl2@illinois.edu>
2019-11-27 15:36:01 -0600

    Get Weather data version 1.0
    Signed-off-by: Giang Le <gianghl2@illinois.edu>

--- app/src/main/java/com/example/myfirstapp/Weather.java
+++ app/src/main/java/com/example/myfirstapp/Weather.java
@@ -3,15 +3,15 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
-import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Weather extends AppCompatActivity {
@@ -19,37 +19,56 @@ public class Weather extends AppCompatActivity {
     /** The URL of the weather query result. */
     static final String URL = "http://api.openweathermap.org/data/2.5/weather?id=4914570&APPID=57e68cf6582d83cfb852527e4c18e9e5&units=imperial";
 
+    /** Variable to hold the temperature from weather data. */
+    private double temperature;
+
+    /** Variable to hold the wind speed from weather data. */
+    private double windSpeed;
+
+    /** TextView to hold weather's description. */
+    private TextView weatherDescription;
+
+    /** Reference to the weather description. */
+    private TextView description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        weatherDescription = (TextView) findViewById(R.id.description);
+        getWeather();
     }
 
     /**
      * Method to get weather info from openweathermap.
      */
     public void getWeather() {
-        Log.d("MyApp","I am here");
 
         JsonObjectRequest objectRequest = new JsonObjectRequest(JsonObjectRequest.Method.GET,
                 URL, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                //{"coord":{"lon":-88.21,"lat":40.11},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":49.28,"pressure":1008,"humidity":87,"temp_min":46,"temp_max":52},"visibility":16093,"wind":{"speed":10.29,"deg":130},"clouds":{"all":90},"dt":1574791169,"sys":{"type":1,"id":6026,"country":"US","sunrise":1574772634,"sunset":1574807401},"timezone":-21600,"id":4914570,"name":"Urbana","cod":200}
                 try {
-                    JSONObject main = response.getJSONObject("main");
-                    JSONArray weatherArray = main.getJSONArray("weather");
-                   // JSONObject weatherObject = weatherArray.getJSONObject()
-                    System.out.println(main);
-                } catch (JSONException e) {
+                    JSONObject mainOb = response.getJSONObject("main");
+                    temperature = mainOb.getDouble("temp");
+                    JSONArray weatherArray = response.getJSONArray("weather");
+                    JSONObject weatherOb = weatherArray.getJSONObject(0);
+                    String descriptionString = weatherOb.getString("description");
+                    JSONObject wind = response.getJSONObject("wind");
+                    windSpeed = wind.getDouble("wind");
+                    weatherDescription.setText(descriptionString);
+                } catch (Exception e) {
                     System.out.println(e);
                 }
+                
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                return;
+                error.printStackTrace();
             }
         });
+        RequestQueue newRequest = Volley.newRequestQueue(this);
+        newRequest.add(objectRequest);
     }
 }
