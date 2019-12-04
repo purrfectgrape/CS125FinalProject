a91da6af750ef1d1212b01d284d435fc7ca98980
Rohit Rahim <hrahim2@illinois.edu>
2019-12-03 19:03:47 -0600

    Changes to the MyClosetUI

--- app/src/main/java/com/example/myfirstapp/Shoes.java
+++ app/src/main/java/com/example/myfirstapp/Shoes.java
@@ -1,10 +1,17 @@
 package com.example.myfirstapp;
 
+import java.util.List;
+
 import android.graphics.Color;
 
 public class Shoes extends Clothing {
 
+    List<Clothing>
+
     Shoes(String type, Color color) {
         super(type, color);
     }
+
+
+
 }
