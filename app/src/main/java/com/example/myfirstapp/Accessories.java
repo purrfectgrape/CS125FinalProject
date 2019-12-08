e9dace185afe5929bd6b5d82c8b119e19c830a26
Giang Le <gianghl2@illinois.edu>
2019-12-08 01:27:23 -0600

    Add multiple pictures
    Signed-off-by: Giang Le <gianghl2@illinois.edu>

--- app/src/main/java/com/example/myfirstapp/Accessories.java
+++ app/src/main/java/com/example/myfirstapp/Accessories.java
@@ -6,44 +6,51 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class Accessories extends AppCompatActivity {
 
-    private ImageView myImage;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    private Button addAccessories;
-    private Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    private static final int CAMERA_REQUEST = 1888;
+
+    private Button addButton;
+    private LinearLayout myLayout;
+    private ImageView image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accessories);
-        addAccessories = (Button) findViewById(R.id.addButton);
-        addAccessories.setOnClickListener(new View.OnClickListener() {
+
+        addButton = (Button) findViewById(R.id.addButton);
+        image = (ImageView) findViewById(R.id.imageView);
+        myLayout=(LinearLayout)findViewById(R.id.layout);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                dispatchTakePictureIntent();
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
         });
     }
 
-    // The below code was taken from Google Developers' Page.
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView toReturnImageView = findViewById(R.id.imageView);
-            toReturnImageView.setImageBitmap(imageBitmap);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            image = new ImageView(this);
+            LayoutParams param = new LayoutParams(300, 300);
+            image.setLayoutParams(param);
+            image.setImageBitmap(photo);
+            image.setTag(java.util.UUID.randomUUID().toString());
+            myLayout.addView(image);
         }
     }
+
 }
