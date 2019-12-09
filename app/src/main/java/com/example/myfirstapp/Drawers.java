f7b78061a91d0018e3e81b4f871388110f49ed81
Rohit Rahim <hrahim2@illinois.edu>
2019-12-08 22:42:41 -0600

    Camera stuff to the clothing objects

--- app/src/main/java/com/example/myfirstapp/Drawers.java
+++ app/src/main/java/com/example/myfirstapp/Drawers.java
@@ -2,13 +2,69 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class Drawers extends AppCompatActivity {
+
+
+    private static final int CAMERA_REQUEST = 1888;
+
+    private Button addButton;
+    private Button removeButton;
+    private LinearLayout myLayout;
+    private ImageButton image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawers);
+
+
+        addButton = (Button) findViewById(R.id.addButton);
+        removeButton = (Button) findViewById(R.id.removeButton);
+        image = (ImageButton) findViewById(R.id.imageView);
+        myLayout = (LinearLayout) findViewById(R.id.layout);
+
+        image.setVisibility(View.INVISIBLE);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myLayout.removeView(image);
+            }
+        });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            image = new ImageButton(this);
+            LayoutParams param = new LayoutParams(300, 300);
+            image.setLayoutParams(param);
+            image.setImageBitmap(photo);
+            image.setTag(java.util.UUID.randomUUID().toString());
+            myLayout.addView(image);
+        }
+    }
+
+
 }
