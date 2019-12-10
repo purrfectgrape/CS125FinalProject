ae5fdb872557cb97b07d61fcc6b5ce2a3e0a6bff
Rohit Rahim <hrahim2@illinois.edu>
2019-12-09 18:43:40 -0600

    Camera stuff to the clothing objects

--- app/src/main/java/com/example/myfirstapp/MyCloset.java
+++ app/src/main/java/com/example/myfirstapp/MyCloset.java
@@ -33,8 +33,8 @@ public class MyCloset extends AppCompatActivity {
             }
         });
 
-        bottomsButton = (Button) findViewById(R.id.bottomsButton);
-        shoesButton.setOnClickListener(new View.OnClickListener() {
+        bottomsButton = (Button) findViewById(R.id.boTTomsButton);
+        bottomsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openMyBottoms();
